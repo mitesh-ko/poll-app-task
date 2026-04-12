291d23ae152401d6c0238e020095ffd8cef9aa8d
Mitesh <72382861+mitesh-ko@users.noreply.github.com>
2026-04-12 15:35:32 +0530

    feat: Database structure

---
+++ database/migrations/2026_04_12_083839_create_polls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('polls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('question');
+            $table->text('description')->nullable();
+            $table->string('slug')->unique()->index(); // datatype is varchar for better performance
+            $table->unsignedTinyInteger('is_multichoice')->default(0);
+            $table->timestamp('end_at')->nullable();
+            $table->json('data')->comment('store answer summary and other malicious data'); // implement DTO to ensure only verified data keys can store.
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('polls');
+    }
+};
---
+++ database/migrations/2026_04_12_092357_create_poll_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        // model name is answer to use general term
+        Schema::create('poll_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable(); // guest user can submit answer
+            $table->foreignId('poll_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('poll_option_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('poll_answers');
+    }
+};
