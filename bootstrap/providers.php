d14345a36f22facf517b04faf67883ce27bf3889
Mitesh <72382861+mitesh-ko@users.noreply.github.com>
2026-04-13 16:21:00 +0530

    fix: notinclude telescope in provider on production

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,8 +1,13 @@
 <?php
-
-return [
+$providers = 
+[
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
 ];
+
+if (app()->environment('local')) {
+    $providers[] = App\Providers\TelescopeServiceProvider::class;
+}
+
+return $providers;
