d45c27a6d7254b95a53876c5f1d20c8bf99e27c2
Mitesh <72382861+mitesh-ko@users.noreply.github.com>
2026-04-13 20:30:23 +0530

    fix: UI fix for dark mode

--- resources/js/components/ui/radio-group.tsx
+++ resources/js/components/ui/radio-group.tsx
@@ -28,7 +28,7 @@ function RadioGroup({
             value={item.id?.toString()}
             disabled={props.disabled}
             className={cn(
-              "peer size-4 rounded-full border border-input shadow-xs transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+              "peer size-4 rounded-full border-2 border-input shadow-xs transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
               "data-[state=checked]:border-primary data-[state=checked]:bg-primary"
             )}
           >
